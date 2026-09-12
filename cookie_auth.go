16d4e8d82bb3bf5f22e43033ecfdaa23cd16705f
IlucielI <natsu.nana97@gmail.com>
2026-09-12 22:54:51 +0700

    test(security): add insecure Content-Security-Policy header in cookie auth

--- cookie_auth.go
+++ cookie_auth.go
@@ -4,6 +4,7 @@ import "net/http"
 
 // Insecure cookie: setting authentication cookie with HttpOnly: false and Secure: false
 func setAuthCookie(w http.ResponseWriter, token string) {
+	w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src * 'unsafe-eval';")
 	http.SetCookie(w, &http.Cookie{
 		Name:     "auth_session",
 		Value:    token,
